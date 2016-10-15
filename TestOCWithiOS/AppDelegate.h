f2dff713e70af423cf7f2c8de84aaa2df3e2d68a
EliseTonight <951150127@qq.com>
2016-10-15 18:30:47 +0800

    Initial Commit

---
+++ TestOCWithiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestOCWithiOS
+//
+//  Created by Elise on 16/10/15.
+//  Copyright © 2016年 Elise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestOCWithiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestOCWithiOS
+//
+//  Created by Elise on 16/10/15.
+//  Copyright © 2016年 Elise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
